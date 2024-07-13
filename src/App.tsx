88f2b150b441a4fe31bce74d8e4218f9fb5f8593
Tim Stanton <thstanton@proton.me>
2024-07-13 22:05:02 +0100

    Mobile friendly

--- src/App.tsx
+++ src/App.tsx
@@ -52,12 +52,12 @@ function App() {
       <div className="absolute -z-10 h-screen w-screen bg-gradient-to-b from-stone-800 via-stone-700/40 to-yellow-300/70"></div>
       <div className="flex h-full w-full flex-col items-center justify-evenly p-4">
         <div>
-          <h1 className="font-unica text-center text-6xl font-bold text-stone-300/60 drop-shadow">
+          <h1 className="pb-2 text-center font-unica text-6xl font-bold text-stone-300/60 drop-shadow">
             The Poison Marshes
           </h1>
-          <div className="font-unica flex gap-5 text-stone-300/60">
+          <div className="flex gap-5 font-unica text-stone-300/60">
             <div>
-              <span className="countdown text-8xl font-bold">
+              <span className="countdown text-6xl font-bold sm:text-8xl">
                 <span
                   style={
                     {
@@ -69,7 +69,7 @@ function App() {
               days
             </div>
             <div>
-              <span className="countdown text-8xl font-bold">
+              <span className="countdown text-6xl font-bold sm:text-8xl">
                 <span
                   style={
                     {
@@ -81,7 +81,7 @@ function App() {
               hours
             </div>
             <div>
-              <span className="countdown text-8xl font-bold">
+              <span className="countdown text-6xl font-bold sm:text-8xl">
                 <span
                   style={
                     {
@@ -93,7 +93,7 @@ function App() {
               min
             </div>
             <div>
-              <span className="countdown text-8xl font-bold">
+              <span className="countdown text-6xl font-bold sm:text-8xl">
                 <span
                   style={
                     {
@@ -106,22 +106,25 @@ function App() {
             </div>
           </div>
         </div>
-        <div className="flex -rotate-2 flex-col items-center justify-center gap-8 rounded-lg bg-stone-300/90 p-8 drop-shadow-xl">
-          <div className="font-rock text-center text-3xl font-bold text-stone-700">
+        <div className="flex w-11/12 -rotate-2 flex-col items-center justify-center gap-8 rounded-lg bg-stone-300/90 p-4 drop-shadow-xl md:w-full md:p-8">
+          <div className="text-center font-rock text-lg font-bold text-stone-700 md:text-3xl">
             <p>We need your help to save the village.</p>
             <p>Find out more - soon</p>
           </div>
-          <form action="" className="flex gap-2">
+          <form action="" className="mx-2 flex flex-col gap-2 md:flex-row">
             <input
               type="email"
               className="input input-bordered bg-transparent text-stone-200"
             />
-            <button className="btn border-0 bg-stone-400" type="submit">
+            <button
+              className="btn border-0 bg-stone-400 text-neutral"
+              type="submit"
+            >
               Join Us
             </button>
           </form>
         </div>
-        <div className="font-special absolute bottom-0 w-full py-4 text-center">
+        <div className="absolute bottom-0 w-full py-4 text-center font-special text-neutral">
           <p>
             The Poison Marshes is part of{" "}
             <a href="https://alrewas-artsfest.co.uk" target="_blank">
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,4 +15,7 @@ export default {
     },
   },
   plugins: [daisyui],
+  daisyui: {
+    themes: ["dark"],
+  },
 };
