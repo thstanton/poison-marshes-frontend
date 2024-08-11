dcc7b1fe7296c1c7c7d87ffea7616428ff429d72
Tim Stanton <thstanton@proton.me>
2024-08-12 00:21:12 +0100

    Reverse proxy configuration

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="vite/client" />
+
+declare const __APP_ENV__: string;
+declare const __BASE_URL__: string;
+declare const __DEV_BASE_URL__: string;
+declare const __PROD_BASE_URL__: string;
--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,29 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
 
-export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [tailwindcss()],
-    },
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    css: {
+      postcss: {
+        plugins: [tailwindcss()],
       },
     },
-  },
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+    define: {
+      __APP_ENV__: env.VITE_VERCEL_ENV,
+      __BASE_URL__: env.VITE_API_URL,
+    },
+  };
 });
